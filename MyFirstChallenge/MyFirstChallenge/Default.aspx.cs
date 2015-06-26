f82395c68e9d9408de4fb3cd754ac002654579a8
Dainichi <dainichi@me.com>
2015-06-26 01:40:41 -0400

    Completed Challenge 1 First Version

---
+++ MyFirstChallenge/MyFirstChallenge/Default.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MyFirstChallenge
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        // MyFirstChallenge
+        // Declare 2 variables set them to value of TextBox .Text
+        protected void clickMeButton_Click(object sender, EventArgs e)
+        {
+            string ageQuery = ageQueryTextBox.Text;
+            string moneyInPocket = moneyQueryTextBox.Text;
+
+            string result = "At " + ageQuery + " one would expect you would have "+
+                                            "more than " + moneyInPocket + " in your pocket";
+            resultLabel.Text = result;
+        }
+    }
+}
